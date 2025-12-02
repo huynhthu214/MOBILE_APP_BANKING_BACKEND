1f48df97335586da2b64b374790e81ab5a18ec03
thuyvi48 <phanthuyvi1004@gmail.com>
2025-12-02 22:48:30 +0700

    payment

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from routes.auth import bp as auth_bp
 from routes.users import bp as users_bp
+from routes.payments import bp as payments_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(users_bp)
+app.register_blueprint(payments_bp)   
+
 if __name__ == "__main__":
     app.run(debug=True)
