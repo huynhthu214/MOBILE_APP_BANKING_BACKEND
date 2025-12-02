9081e09721a2593de5c2f5027548a3f1c09e567a
huynhthu214 <minhthuhuynh23@gmail.com>
2025-12-02 22:21:23 +0700

    cài docker

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,13 @@
+import os
 import pymysql
 
 def get_conn():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="zybank",
-        cursorclass=pymysql.cursors.DictCursor  # rất quan trọng
+        host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        database=os.getenv("DB_NAME", "zybank"),
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit=True
     )
