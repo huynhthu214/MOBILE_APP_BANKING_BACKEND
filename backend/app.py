e4cd57868abae5d6521f43bd1f7bbaa25257dfea
thuyvi48 <phanthuyvi1004@gmail.com>
2025-12-05 23:13:59 +0700

    utility payment

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from routes.auth import bp as auth_bp
 from routes.users import bp as users_bp
 from routes.payments import bp as payments_bp
 from routes.transactions import bp as transactions_bp
+from routes.utility import bp as utility_bp
 from routes.ekyc import bp as ekyc_bp
 from routes.files import bp as files_bp
 from routes.otp import bp as otp_bp
@@ -13,9 +14,10 @@ from routes.system import bp as system_bp
 app = Flask(__name__)
 
 app.register_blueprint(auth_bp)
-app.register_blueprint(users_bp)
+app.register_blueprint(users_bp)    
 app.register_blueprint(payments_bp)   
 app.register_blueprint(transactions_bp) 
+app.register_blueprint(utility_bp)
 app.register_blueprint(ekyc_bp)
 app.register_blueprint(files_bp)
 app.register_blueprint(otp_bp)
